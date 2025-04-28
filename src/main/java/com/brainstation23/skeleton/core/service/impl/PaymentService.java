6ab85e89a7139f5a2463d7bc0c4cd9735e0ac7ce
navid1512 <navid.razzaque@brainstation-23.com>
2025-04-28 18:45:40 +0600

    added request money status

--- src/main/java/com/brainstation23/skeleton/core/service/impl/PaymentService.java
+++ src/main/java/com/brainstation23/skeleton/core/service/impl/PaymentService.java
@@ -1,20 +1,26 @@
 package com.brainstation23.skeleton.core.service.impl;
 
+import com.brainstation23.skeleton.core.domain.enums.RequestMoneyStatusEnum;
 import com.brainstation23.skeleton.core.domain.enums.ResponseMessage;
 import com.brainstation23.skeleton.core.domain.exceptions.RecordNotFoundException;
 import com.brainstation23.skeleton.core.service.BaseService;
+import com.brainstation23.skeleton.data.entity.ExpenseSplit;
 import com.brainstation23.skeleton.data.entity.expense.IndividualEventExpense;
+import com.brainstation23.skeleton.data.repository.ExpenseSplitRepository;
 import com.brainstation23.skeleton.data.repository.expense.IndividualEventExpenseRepository;
 import com.brainstation23.skeleton.presenter.domain.request.user.PaymentRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PaymentService extends BaseService {
 
     private final IndividualEventExpenseRepository individualEventExpenseRepository;
+    private final ExpenseSplitRepository expenseSplitRepository;
 
     @Transactional
     public void initiatePaymentProcess(PaymentRequest paymentRequest) {
@@ -24,7 +30,22 @@ public class PaymentService extends BaseService {
         String eventId = paymentRequest.getEventId();
         updateSenderIndividualExpenseReport(senderUsername,amount,eventId);
         updateReceiverIndividualExpenseReport(receiverUsername,amount,eventId);
+        updateExpenseSplitRequestStatus(senderUsername,receiverUsername,eventId);
         //TODO: update payment history status
+    }
+
+    @Transactional
+    public void updateExpenseSplitRequestStatus(String senderUsername, String receiverUsername, String eventId) {
+        final Optional<ExpenseSplit> expenseSplitOptional
+                = expenseSplitRepository.findByEventIdAndSenderUserNameAndReceiverUserName(
+                eventId,
+                senderUsername,
+                receiverUsername);
+
+        if (expenseSplitOptional.isPresent())
+        {
+            expenseSplitOptional.get().setRequestStatus(RequestMoneyStatusEnum.ACCEPTED.toString());
+        }
     }
 
     @Transactional
